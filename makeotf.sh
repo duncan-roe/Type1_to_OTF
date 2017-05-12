15d0ec26be8623ba32c5990b4c7c87359deda60c
duncan-roe <duncan_roe@optusnet.com.au>
2017-05-12 15:02:18 +1000

    Add the 2 files that actually do the work

---
+++ makeotf.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+makeotf -f c0648bt_.pfb
+makeotf -f c0632bt_.pfb -b
+makeotf -f c0633bt_.pfb -i -b
+makeotf -f c0649bt_.pfb -i
+makeotf -f cour.pfa
+makeotf -f courb.pfa  -b
+makeotf -f courbi.pfa -i -b
+makeotf -f couri.pfa  -i
+makeotf -f c0419bt_.pfb
+makeotf -f c0583bt_.pfb -b
+makeotf -f c0611bt_.pfb -i -b
+makeotf -f c0582bt_.pfb -i
+makeotf -f UTRG____.pfa
+makeotf -f UTB_____.pfa -b
+makeotf -f UTBI____.pfa -i -b
+makeotf -f UTI_____.pfa -i
+makeotf -f c059013l.pfb
+makeotf -f c059016l.pfb -b
+makeotf -f c059036l.pfb -i -b
+makeotf -f c059033l.pfb -i
+makeotf -f a010013l.pfb
+makeotf -f a010015l.pfb -b
+makeotf -f c059013l.pfb
+makeotf -f c059016l.pfb -b
+makeotf -f b018012l.pfb
+makeotf -f b018015l.pfb -b
+makeotf -f n019003l.pfb
+makeotf -f n019004l.pfb -b
+makeotf -f n019024l.pfb -i -b
+makeotf -f n019023l.pfb -i
+makeotf -f n019043l.pfb
+makeotf -f n019044l.pfb -b
+makeotf -f n021003l.pfb
+makeotf -f n022003l.pfb
+makeotf -f n022004l.pfb -b
+makeotf -f n022024l.pfb -i -b
+makeotf -f n022023l.pfb -i
+makeotf -f p052003l.pfb
+makeotf -f p052004l.pfb -b
+makeotf -f z003034l.pfb -i
